3ced22da31802d7bb06f24995395ac0390bb18e6
Tim Lamborn <tlamborn@gmail.com>
2025-05-21 00:59:19 +1200

    Expands briefcase item list

--- char-sheet/src/data/briefcase.tsx
+++ char-sheet/src/data/briefcase.tsx
@@ -64,11 +64,20 @@ const briefcaseRuleset: Rules = {
             description: "Armored vs empathy rolls.",
         },
         {
-            name: 'Headset',
+            name: 'In-ear headset',
+            description: "A blue led blinks on the side. Its unclear whether you are on a call.",
         },
         {
             name: 'Stiletto heels',
             description: "Incredibly loud.",
+        },
+        {
+            name: 'Makeup kit',
+            description: "A small compact with a mirror. The foundation is nearly exhausted.",
+        },
+        {
+            name: 'Lipstick',
+            description: "Red #6. Bold. Provocative. Adheres to a surprising number of surfaces.",
         },
         {
             name: 'Jeweled broach',
@@ -76,7 +85,7 @@ const briefcaseRuleset: Rules = {
         },
         {
             name: 'Rolex watch',
-            description: "Almost certainly fake. The time is incorrect.",
+            description: "Almost certainly fake. It reads one hour early.",
         },
         {
             name: 'Three piece suit',
@@ -93,10 +102,11 @@ const briefcaseRuleset: Rules = {
         },
         {
             name: 'Hand computer',
+            description: "State of the art computing in a modern form factor. It is crippled by the IT department.",
         },
         {
             name: 'Calculator',
-            description: "Very old school. It was incredibly expensive.",
+            description: "Minimal, yet functional. It displays a truth to materials.",
         },
         {
             name: 'Notebook',
@@ -104,6 +114,7 @@ const briefcaseRuleset: Rules = {
         },
         {
             name: 'Clipboard',
+            description: "Light, yet sturdy. A pen hangs from a piece of string. One corner is chipped.",
         },
         {
             name: 'Box of tissues',
@@ -112,22 +123,25 @@ const briefcaseRuleset: Rules = {
         },
         {
             name: 'Stapler',
+            description: "A Swingline 747 in lipstick red. A classic.",
         },
         {
             name: 'Binder',
+            description: "A black 3 ring binder. The label has long since faded.",
         },
         {
             name: 'Duct tape',
+            description: "Very difficult to tear. Leaves a sticky residue.",
             stack: 4,
         },
         {
-            name: 'Perment marker',
+            name: 'Permanent marker',
+            description: "Comes in a variety of fun colors!",
             stack: 6,
-            description: "Comes in a variety of colors!",
         },
         {
             name: 'Laser pointer',
-            description: "Nobody uses these anymore.",
+            description: "People stopped using these ages ago.",
         },
         {
             name: 'Really nice pen',
@@ -136,9 +150,11 @@ const briefcaseRuleset: Rules = {
         },
         {
             name: 'Screwdriver set',
+            description: "Includes multiples sizes of flathead and phillips. No torx.",
         },
         {
             name: 'Business card',
+            description: "Look at that subtle off while coloring. The tasteful thickness of it. It even has a watermark.",
             stack: 10,
         },
         {
@@ -147,26 +163,44 @@ const briefcaseRuleset: Rules = {
         },
         {
             name: 'Corporate branding guide',
+            description: "Know your monograms from your letterforms."
         },
         {
             name: 'Pantone color palette',
+            description: "Looking at this incurrs a fee.",
         },
         {
             name: 'Technical manual',
+            description: "Makes a satisfying thump when closed.",
+        },
+        {
+            name: 'Saucy novel',
+            description: "Definitely not safe for work.",
         },
         {
             name: 'Scented candle',
+            description: "Smells of cinnamon and vanilla.",
         },
         {
             name: 'Packed lunch',
             description: "This probably needs to be microwaved.",
         },
         {
+            name: 'A can of tuna',
+            stack: 3,
+        },
+        {
+            name: 'Kitten',
+            description: "This is not a pet friendly workplace.",
+        },
+        {
             name: 'Box of donuts',
+            description: "A corporate sanctioned way to express favoritism.",
             stack: 8,
         },
         {
             name: 'Data chip',
+            description: "Contains K-drama episodes and some strange executable files.",
             stack: 4,
         },
         {
@@ -178,27 +212,49 @@ const briefcaseRuleset: Rules = {
             stack: 10,
         },
         {
+            name: 'Site safety forms',
+            description: "Currently unfilled.",
+            stack: 10,
+        },
+        {
+            name: 'Performance review forms',
+            description: "Powerful vs junior employees.",
+            stack: 10,
+        },
+        {
             name: 'Company bonds',
             stack: 10,
         },
         {
             name: 'Sticky notes',
+            description: "A reliable way to remember your login details.",
             stack: 10,
         },
         {
             name: 'Schematics',
+            description: "You could make these. If only you had some elemental silicon, a nano-scale CNC mill, and a semiconductor foundry.",
             stack: 4,
         },
         {
             name: 'Blueprints',
-            stack: 4,
+            description: "This seems to be the office floorplan. The roof contains a surprising number of ventilation ducts.",
+            stack: 2,
         },
         {
             name: 'Incriminiating photo',
             stack: 10,
         },
         {
+            name: 'Hi-vis vest',
+            description: "A bright yellow vest with reflective strips. Visibility guarantees safety.",
+        },
+        {
             name: 'Cocaine',
+            stack: 4,
+        },
+        {
+            name: 'Bleach',
+            description: "Comes in a 4L industrial jug.",
             stack: 4,
         },
         {
@@ -206,12 +262,21 @@ const briefcaseRuleset: Rules = {
             description: "Surprisingly heavy.",
             stack: 1,
         },
+        {
+            name: 'Six pack of beer',
+            description: "Room temperature. You do not recognize the brand.",
+            stack: 6,
+        },
+        {
+            name: 'Strange key',
+            description: "The tag reads: 'removing this key is a firable offense.'",
+        },
         ...DEFAULT_RULES.equipment,
     ],
     containers: [
         {
             name: "Personal",
-            size: 3,
+            size: 4,
         },
         {
             name: "Briefcase",
@@ -219,7 +284,7 @@ const briefcaseRuleset: Rules = {
         },
         {
             name: "Desk",
-            size: 5,
+            size: 8,
         },
     ],
     powers: [
